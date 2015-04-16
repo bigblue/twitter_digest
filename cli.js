5b3a1a9e50bf838f62913a36707b8aa1a4f0df46
Michael Vigor <michael@bigbluedev.com>
2015-04-16 18:18:58 +0100

    Save unique link details to mongodb

--- cli.js
+++ cli.js
@@ -6,6 +6,10 @@ var url = require('url');
 var async = require('async');
 var Twitter = require('twitter');
 var _ = require('lodash');
+var mongojs = require('mongojs');
+var db = mongojs(process.env.MONGODB_URI, ['links', 'feed']);
+
+process.setMaxListeners(0);
  
 var client = new Twitter({
   consumer_key: process.env.CONSUMER_KEY,
@@ -15,36 +19,35 @@ var client = new Twitter({
 });
 
 function fetchTweetList(params, tweetsSoFar, cb){
+  console.log("Getting tweets from /search/tweets/json?" + qs.stringify(params))
   client.get('search/tweets', params, function(error, tweets, response){
+    if(!tweets.statuses || tweets.statuses.length == 0){
+      return cb(tweetsSoFar)
+    }
+
     var statuses = _.pluck(_.filter(tweets.statuses, function(s){ return !s.retweeted_status }), 'text')
     var allTweets = tweetsSoFar.concat(statuses)
+    if(!params.max_id){
+      db.feed.update({ feedState: 'since_id' },
+                      {
+                        feedState: 'since_id',
+                        value: tweets.search_metadata.max_id_str
+                      },
+                      {upsert: true},
+                      function(err, doc){  })
+    }
 
-    var searchFromTime = moment().subtract(1, 'days');
+    var searchFromTime = moment().subtract(2, 'hours');
     var lastTweetTime = moment(_.last(tweets.statuses).created_at)
 
     if(tweets.search_metadata.next_results && (lastTweetTime > searchFromTime)){
       var nextPageParams = qs.parse(tweets.search_metadata.next_results.replace(/^\?/, ""))
-      console.log(nextPageParams)
       fetchTweetList(nextPageParams, allTweets, cb)
     } else {
       cb(allTweets)
     }
   });
 }
-
-var searchTerms = process.argv.slice(2).join(" ");
-
-var params = {q: 'filter:links ' + searchTerms,
-              lang: 'en',
-              result_type: 'recent',
-              count: 100,
-              include_entities: false
-}
-
-//fetchTweetList(params, [], function(list){
-//  console.log(list)
-//  console.log(list.length)
-//});
 
 function getTweetLinks(status){
   var re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/g; 
@@ -74,11 +77,13 @@ function getPageTitle(body){
 
 function followLink(link, statuses, cb){
   var r = request({uri: link,
-                   followAllRedirects: true}, 
+                   followAllRedirects: true,
+                   maxRedirects: 8}, 
                   function (err, res, body) {
     if (!err && res.statusCode == 200) {
       var uri = url.parse(r.uri.href)
       var linkKey = uri.protocol + '//' + uri.host + uri.pathname;
+      console.log('Link ' + link + ' followed to ' + linkKey)
       var fullLink = uri.protocol + '//' + uri.host + uri.path;
       var title = getPageTitle(body);
       linkData = {}
@@ -98,7 +103,8 @@ function splitTweetLinks(tweets){
 }
 
 function followTweetLinks(splitLinkTweets, complete){
-  async.map(_.pairs(splitLinkTweets), function(t, cb){
+  console.log(_.keys(splitLinkTweets).length + " links to follow...")
+  async.mapLimit(_.pairs(splitLinkTweets), 10, function(t, cb){
     followLink(t[0], t[1], cb)
   }, function(err, results){
    var combined = _.reduce(results, function(acc, link){
@@ -115,20 +121,56 @@ function followTweetLinks(splitLinkTweets, complete){
   })
 }
 
-var tweets = ['.@Hortonworks buys SequenceIQ, to speed cloud- and #Docker-based #Hadoop deployments #BigDataCo http://t.co/DfxhdViFrh',
-  'Docker Attracts Big Fish, Eyes Micro-Service Platform http://t.co/skxA2dSw1C #fundraising',
-  'Docker Said to Join $1 Billion Valuation Club With New Funding http://t.co/WexN1B8WG1 #fundraising',
-  'Deploying Perl Docker Container to Elastic Beanstalk http://t.co/GgwnO9BxQE #aws #StackOverflow',
-  'Docker Raises $95M in Series D #Funding http://t.co/BEnmtucAQ9',
-  'Modern DevOps with Docker http://t.co/o1rmWqIQgl #Docker #ITOps',
-  '.@andypiper this post on the @xebialabs blog seemed like an interesting perspective on such things http://t.co/zYwZFPGH9w @chanezon',
-  'So fun to see almost what the Docker logo turned out to be.  https://t.co/9g2P2EyUKS',
-  'Simple TOSCA Orchestration for Docker x @CloudOpting  http://t.co/uonh8n2XVY',
-  'Docker Said to Join $1 Billion Valuation Club With New Funding http://t.co/WexN1B8WG1 #fundraising',
-  '#Docker for #Node.js and Puppet\'s Chocolatey - #DevOps News Digest http://t.co/necKeDvIH0',
-  'Test tweet http://bit.ly/1IhnC7X']
+function setupDb(cb){
+  db.links.ensureIndex({linkId: 1}, {unique: true}, cb)
+}
+
+function saveLinks(tweetsWithLinks, cb){
+  var docs = _.map(_.pairs(tweetsWithLinks), function(t){
+    return {linkId: t[0],
+            fullLink: t[1][0],
+            pageTitle: t[1][1],
+            statuses: t[1][2]}
+  })
+
+  async.each(docs, function(doc, callback){
+    db.links.update({ linkId: doc.linkId },
+                    {
+                      '$setOnInsert': _.omit(doc, 'statuses'),
+                      '$addToSet': { statuses: { '$each': doc.statuses }}
+                    },
+                    {upsert: true},
+                    function(err, doc){ 
+                      if(err){
+                        console.log(err)
+                      }
+                      callback(err) })
+  }, cb)
+}
+
+var searchTerms = process.argv.slice(2).join(" ");
+
+var params = {q: 'filter:links ' + searchTerms,
+              lang: 'en',
+              result_type: 'recent',
+              count: 100,
+              include_entities: false
+}
+
+db.feed.findOne({feedState: 'since_id'}, function(err, since){
+  if (since) {
+    params['since_id'] = since.value
+  }
+  fetchTweetList(params, [], function(list){
+    setupDb(function(err, doc){
+      followTweetLinks(splitTweetLinks(list), function(results){
+        console.log(_.keys(results).length + " links to save... ")
+        saveLinks(results, function(err){
+          db.close()
+        });
+      })
+    })
+  });
+});
 
 
-followTweetLinks(splitTweetLinks(tweets), function(results){
-  console.log(_.values(results))
-})
